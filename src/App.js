60095295507d78ab49f072a1e9d127fef296204c
JimmyBicycleLover <jimmy99193179@gmail.com>
2023-10-20 14:46:42 +0800

    project first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense'
+import Filter from './components/Expenses/ExpensesFilter'
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Insurance",
+    amount: 100,
+    date: new Date(2022, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Rent",
+    amount: 250,
+    date: new Date(2020, 2, 28),
+  },
+  {
+    id: "e3",
+    title: "New Iphone",
+    amount: 1200,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
+  const[expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = expense => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+      // [新加入的, 原本就存在的]
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
